2ef15cc2948462121bdcad0b1f45b9d95ad99595
Вика <vika.ladyhina@gmail.com>
2025-09-20 13:58:06 +0200

    theme-refactor

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
 import LangSelector from '../langSelector/LangSelector'
+import ThemeToggle from '../themeToggle/ThemeToggle'
 
 export const Header = () => {
 
   return (
     <div className='header'>
         <img src="/title.gif" alt="" />
+        <ThemeToggle />
         <LangSelector />
     </div>
   )
