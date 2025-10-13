716a95085dd0032f41d62627c5d6a46b18c910cc
Вика <vika.ladyhina@gmail.com>
2025-10-13 11:08:29 +0200

    Fixed bugs for deployment, translations, and the sitemap

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router'
+import './Footer.scss'
+
+const Footer = () => {
+
+    
+  return (
+    <div className='footer'>
+      <div className='footer_founders'>Основатели - Виктория и Павел Ладыгины
+        <div className='footer_founders'>our.travels1979@gmail.com</div>
+      </div>
+      <div className='fooer_map'>
+        <Link to={`/map`}>Карта сайта</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
