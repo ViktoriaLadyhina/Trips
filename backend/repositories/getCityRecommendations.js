350fc1dc599ac184b5f14673f850c352f4faa231
Вика <vika.ladyhina@gmail.com>
2026-07-20 18:25:32 +0200

    feat: add attraction API and city recommendations

--- backend/repositories/getCityRecommendations.js
+++ backend/repositories/getCityRecommendations.js
@@ -2,28 +2,35 @@ async function getCityRecommendations(db, entityId, lang) {
     const [rows] = await db.query(
         `
         SELECT
-            e.id,
-            e.path,
-            c.content AS name,
-            p.id AS photo_id,
-            p.path AS photo
-        FROM entities e
+    e.id,
+    e.path,
+    c.content AS name,
+    p.path AS photo,
+    JSON_ARRAYAGG(a.value) AS types -- собирает несколько значений в одно
+FROM entities e
 
-        JOIN content c
-            ON c.entity_id = e.id
-            AND c.block_key = 'name'
-            AND c.language = ?
+JOIN content c
+    ON c.entity_id = e.id
+    AND c.block_key = 'name'
+    AND c.language = ?
 
-        JOIN entity_photos p
-            ON p.entity_id = e.id
-            AND p.is_main = 1
+JOIN entity_photos p
+    ON p.entity_id = e.id
+    AND p.is_main = 1
 
-        WHERE e.parent_id = ?
-            AND e.type = 'attraction'
-            AND e.is_active = 1
+LEFT JOIN entity_attributes a
+    ON a.entity_id = e.id
+    AND a.attribute_group = 'type'
 
-        ORDER BY RAND()
-        LIMIT 9
+WHERE e.parent_id = ?
+    AND e.type = 'attraction'
+    AND e.is_active = 1
+
+GROUP BY
+    e.id,
+    e.path,
+    c.content,
+    p.path;
         `,
         [lang, entityId]
     );
