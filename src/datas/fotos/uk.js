58fd1a642b36c69d5ae200681003a6f32887bd12
Вика <vika.ladyhina@gmail.com>
2026-07-23 14:41:15 +0200

    feat: continue attractions API development

--- src/datas/fotos/uk.js
+++ src/datas/fotos/uk.js
@@ -11,7 +11,7 @@ export const Fotos = {
         { path: "Ukraine/sumska/sumy-city/spaso-preobrazhensky/004.jpg", title: { ru: "Спасо-Преображенский собор, 2010", uk: "Спасо-Преображенський собор, 2010", de: "Spaso-Preobraschenski-Kathedrale, 2010" } },
       ],
       pokrovska_square_sumy: [
-        { path: "Ukraine/sumska/sumy-city/pokrovska_square/altanka/009.jpg", title: { ru: "Альтанка, 2008", uk: "Альтанка, 2008", de: "Der Pavillon, 2008" } },
+        { path: "Ukraine/sumska/sumy-city/pokrovska_square/altanka/001.jpg", title: { ru: "Альтанка, 2008", uk: "Альтанка, 2008", de: "Der Pavillon, 2008" } },
         { path: "Ukraine/sumska/sumy-city/pokrovska_square/onatsky_museum/001.jpg", title: { ru: "Сумской областной художественный музей им. Н. Онацкого, 2010", uk: "Сумський обласний художній музей ім. Н. Онацького, 2010", de: "Oblastliches Kunstmuseum Sumy N. Onatsky, 2010" } },
       ],
       sumy_altanka: [
